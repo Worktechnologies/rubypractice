d157f57eb5bccb529331009b51d24fe7f16cb1f1
Parmjeet Kaur <work.technologies25@gmail.com>
2023-03-27 12:08:32 +1000

    first commit

---
+++ second.rb
@@ -0,0 +1,27 @@
+puts "hello world"
+s=7
+puts "i am doing this 10 times"
+puts "i am string #{s}"
+
+ 10.times do
+    puts"hello world"
+end
+puts
+
+puts "looping with range"
+
+range=1..10
+range.each{|num| puts num}
+
+puts "mapping"
+range.map{|num| puts num}
+
+counter=1
+
+if counter.equal? "1"
+    puts counter
+end
+
+if counter =="1"
+    puts counter
+end
